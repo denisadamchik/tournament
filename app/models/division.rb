c8fef6c01d2ee6ce7c6524eaaaefd0f452355a91
Denis Adamchik <denis.adamchik@gmail.com>
2023-10-06 10:02:22 +0300

    Some helper methods added to Division and Game

--- app/models/division.rb
+++ app/models/division.rb
@@ -8,6 +8,18 @@ class Division < ApplicationRecord
 
   validates :title, inclusion: titles.keys, uniqueness: true
 
-  def process
+  def process = couples.each { Game.play(_1, level:) }
+
+  private
+
+  def couples
+    active_teams = teams.active.order(points: :desc, name: :asc)
+    best_team, worst_team = active_teams.first, active_teams.last
+
+    active_teams.excluding(best_team, worst_team).each_slice(2).to_a << [best_team, worst_team]
   end
+
+  def games = Game.where(home_team: teams).or(Game.where(away_team: teams))
+
+  def level = games.order(:level).last.level
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -10,4 +10,8 @@ class Game < ApplicationRecord
   belongs_to :away_team, class_name: 'Team', foreign_key: 'away_team_id'
 
   validates :level, inclusion: levels.keys, presence: true
+
+  def self.play(couple, level:)
+    Game.create!(home_team: couple.first, away_team: couple.second, level:)
+  end
 end
